da745cade3dfc2cb8a33509a9aae3153e413819c
Maurizio Bonani <maurizio.bonani@vecomp.it>
2017-12-28 22:01:06 +0100

    Removed hmr in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
 var path = require('path')
 var webpack = require('webpack')
 
+var entry = ['./index']
+var plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+]
+
+if (process.env.NODE_ENV === "development") {
+    entry.push('webpack-hot-middleware/client')
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+} else {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }))
+}
+
 module.exports = {
     devtool: '@source-map',
-    entry: [
-        'webpack-hot-middleware/client',
-        './index'
-    ],
+    entry: entry,
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/static/'
     },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': JSON.stringify('production')
-          }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          compressor: {
-            warnings: false
-          }
-        })
-    ],
+    plugins: plugins,
     module: {
         loaders: [
             { test: /\.js$/, loaders: [ 'babel' ], exclude: /node_modules/, include: __dirname },
