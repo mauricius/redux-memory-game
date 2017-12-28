9e1864181a2e27fd2503750ef3b349c0c53244f0
Maurizio Bonani <maurizio.bonani@vecomp.it>
2017-12-28 21:28:03 +0100

    Fix webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
+    devtool: '@source-map',
     entry: [
         'webpack-hot-middleware/client',
         './index'
@@ -15,7 +15,17 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+          }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compressor: {
+            warnings: false
+          }
+        })
     ],
     module: {
         loaders: [
